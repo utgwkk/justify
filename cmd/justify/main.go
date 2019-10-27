8048acae8d5df326dba4a7350cce87a64f396292
UTAGAWA Kiki <utagawakiki@gmail.com>
2019-10-27 16:09:31 +0900

    ascii art mode cli

--- cmd/justify/main.go
+++ cmd/justify/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 
 	"github.com/maxmclau/gput"
@@ -10,10 +13,21 @@ import (
 )
 
 func main() {
+	isAsciiArt := flag.Bool("aa", false, "Enable ascii art mode")
+	flag.Parse()
 	cols := gput.Cols()
-	scanner := bufio.NewScanner(os.Stdin)
 
-	for scanner.Scan() {
-		fmt.Println(justify.Center(cols, scanner.Text()))
+	if *isAsciiArt {
+		buf, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			log.Fatal(err)
+		}
+		input := string(buf)
+		fmt.Println(justify.CenterAsciiArt(cols, input))
+	} else {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			fmt.Println(justify.Center(cols, scanner.Text()))
+		}
 	}
 }
